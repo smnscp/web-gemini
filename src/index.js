27fb78d1d3edfb954463417be81651f0141d6214
Simon Schrape <sim@ilitu.de>
2020-10-18 13:20:22 +0200

    Allow direct level access via URL hash.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import GeminiGame from './gemini_game';
 
+const level = window.location.hash.match(/[#&]level=(\d+)|/)[1]
 const hook = document.querySelector('#gemini_game_container')
-ReactDOM.render(<GeminiGame {...hook.dataset} />, hook)
+ReactDOM.render(<GeminiGame level={level} />, hook)
