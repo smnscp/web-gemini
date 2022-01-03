2ae1921ffba7e94f5bf24b2a845910b3c9608882
Simon Schrape <sim@ilitu.de>
2022-01-03 12:11:32 +0100

    Shorten import name.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import GeminiGameReactComponent from "./view/react/game_component.jsx";
+import GameComponent from "./view/react/game_component.jsx";
 
 const level = window.location.hash.match(/[#&]level=(\d+)|/)[1];
 const hook = document.querySelector("gemini-game");
-ReactDOM.render(<GeminiGameReactComponent level={level} />, hook);
+ReactDOM.render(<GameComponent level={level} />, hook);
