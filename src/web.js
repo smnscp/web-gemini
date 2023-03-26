a2136993d619aa1ee9e5a46dca0a18c21603cd75
Simon Schrape <sim@ilitu.de>
2023-03-26 22:02:43 +0200

    Use proper default value for level.

--- src/web.js
+++ src/web.js
@@ -2,6 +2,6 @@ import GameComponent from "./view/web/game_component.js";
 
 customElements.define("gemini-game", GameComponent, { extends: "div" });
 
-const level = window.location.hash.match(/[#&]level=(\d+)|/)[1];
+const level = window.location.hash.match(/[#&]level=(\d+)|/)[1] || "1";
 const game = document.querySelector("gemini-game");
 game.setAttribute("level", level);
